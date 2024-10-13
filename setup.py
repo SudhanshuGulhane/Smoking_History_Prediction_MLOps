31c16d86456d429838359ab8a90bdd322c6a5a82
Sudhanshu Gulhane <sudhanshu072000@gmail.com>
2024-10-12 18:54:23 -0700

    adding local package and requirements file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="smoking_history_prediction",
+    version="1.0.0",
+    author="Brogrammer",
+    author_email="thorinoakenshielddurin221@gmail.com",
+    packages=find_packages()
+)
